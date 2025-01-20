1d0a00c62a46daa3a89e20a9413d888c0fb37c8b
NieTsuki <nie.0x6708@gmail.com>
2025-01-21 04:07:17 +0800

    Implement Vector2 and Vector3

---
+++ src/lib/structures.ts
@@ -0,0 +1,25 @@
+enum RenderStyle {
+    ARROW,
+    POINT,
+}
+
+export class Vector2 {
+    x: number;
+    y: number;
+    style: RenderStyle;
+
+    constructor(x: number, y: number, style: RenderStyle = RenderStyle.ARROW) {
+        this.x = x;
+        this.y = y;
+        this.style = style;
+    }
+}
+
+export class Vector3 extends Vector2 {
+    z: number;
+
+    constructor(x: number, y: number, z: number, style: RenderStyle = RenderStyle.ARROW) {
+        super(x, y, style);
+        this.z = z;
+    }
+}
