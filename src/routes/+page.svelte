6cc339298ced5e9d04a66d54b184a4f0e85fe4f6
NieTsuki <nie.0x6708@gmail.com>
2025-01-21 16:14:44 +0800

    Implement tabs for 2D and 3D sketch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import P5 from "p5-svelte";
     import type { p5 } from "p5-svelte";
+    import * as Tabs from "$lib/components/ui/tabs";
     import { setup, draw, mouseClicked } from "./sketch";
 
     let sketchParent: HTMLDivElement;
@@ -12,6 +13,21 @@
     }
 </script>
 
-<div bind:this={sketchParent} class="w-screen h-screen">
-    <P5 {sketch} />
-</div>
+<Tabs.Root>
+    <div class="flex justify-center absolute w-full p-4">
+        <Tabs.List class="bg-foreground">
+            <Tabs.Trigger value="2D" class="font-bold">2D</Tabs.Trigger>
+            <Tabs.Trigger value="3D" class="font-bold">3D</Tabs.Trigger>
+        </Tabs.List>
+    </div>
+
+    <div bind:this={sketchParent} class="w-screen h-screen">
+        <Tabs.Content value="2D" class="m-0">
+            <P5 {sketch} />
+        </Tabs.Content>
+
+        <Tabs.Content value="3D" class="m-0">
+            <P5 {sketch} />
+        </Tabs.Content>
+    </div>
+</Tabs.Root>
